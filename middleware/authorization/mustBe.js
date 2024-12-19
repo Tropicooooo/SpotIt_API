d0d45451b0836cd2c1dfe4fafb30e1ffc3fdd3a3
NouridinAT <etu52813@henallux.be>
2024-12-19 01:32:28 +0100

    FINISH

---
+++ middleware/authorization/mustBe.js
@@ -0,0 +1,15 @@
+export const manager = (req, res, next) => {
+    if(req.session.status === 'manager'){
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+};
+
+export const user = (req, res, next) => {
+    if(req.session.status === 'user'){
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
--- model/leaderboard.js
+++
@@ -1,7 +0,0 @@
-export const getLeaderboard = async (SQLClient) => {
-    const { rows } = await SQLClient.query(
-      "SELECT * FROM Users ORDER BY experience DESC LIMIT 10"
-    );
-    return rows;
-  };
-  
