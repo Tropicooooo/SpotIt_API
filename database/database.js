16c6e040812a85635ce4139e52dade6a5fcf1600
Luca <lulu-turrioni@outlook.fr>
2024-12-19 19:54:07 +0100

    fix script sql

--- database/database.js
+++ database/database.js
@@ -5,7 +5,8 @@ const pgPool = new pg.Pool({
     host: process.env.POSTGRES_HOST,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT,
 });
 
 /* ----- Deuxième partie ----- */
