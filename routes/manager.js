2eae1e1949c3e21a91e19ddd84beb1e169090dac
Luca <lulu-turrioni@outlook.fr>
2024-12-20 15:48:49 +0100

    remove swagger comment

--- routes/manager.js
+++ routes/manager.js
@@ -1,11 +1,11 @@
 import Router from 'express-promise-router';
-import { getUsers, getUser, updateUser, deleteUser, createUser } from '../controler/manager.js';
+import { getUsers, getUser,updateUser, deleteUser, createUser } from '../controler/manager.js';
 import { getVouchers, getVoucher, createVoucher, deleteVoucher, updateVoucher } from '../controler/manager.js';
 import { getUserVouchers, getUserVoucher, createUserVoucher, deleteUserVoucher, updateUserVoucher } from '../controler/manager.js';
 import { managerValidatorMiddleware as MPV } from '../middleware/validation.js';
-import { checkJWT } from '../middleware/identification/jwt.js';
-import { manager, employee, admin } from '../middleware/authorization/mustBe.js';
-import { getEmployees, deleteEmployee, updateEmployee, getEmployee, getAllReports, getAllReport, updateReport, createReport, getJobs, getJob, updateJob, deleteReport, getReportTypes, getEmployeesName, updateReportType, createReportType, getAllReportType, deleteReportType, getUsersName } from '../controler/manager.js';
+import {checkJWT} from '../middleware/identification/jwt.js';
+import {manager, employee, admin} from '../middleware/authorization/mustBe.js';
+import { getEmployees, deleteEmployee, updateEmployee, getEmployee, getAllReports, getAllReport, updateReport, createReport,getJobs,getJob,updateJob, deleteReport, getReportTypes, getEmployeesName,updateReportType,createReportType, getAllReportType ,deleteReportType, getUsersName} from '../controler/manager.js';
 import { configureUpload } from '../upload/upload.js';
 import path from 'path';
 
@@ -18,7 +18,6 @@ const REPORTS_FOLDER = path.join(new URL(import.meta.url).pathname, '../../uploa
 const reportUpload = configureUpload(REPORTS_FOLDER);
 
 // Routes pour les utilisateurs
-
 /**
  * @swagger
  * /manager/users:
@@ -48,8 +47,7 @@ const reportUpload = configureUpload(REPORTS_FOLDER);
  *                     description: The user's status.
  *                     example: "user"
  */
-
-router.get('/users', checkJWT, manager, getUsers);
+router.get('/users', checkJWT , manager,  getUsers);
 
 /**
  * @swagger
@@ -78,8 +76,7 @@ router.get('/users', checkJWT, manager, getUsers);
  *                   description: The user's status.
  *                   example: "user"
  */
-
-router.get('/user', checkJWT, manager, getUser);
+router.get('/user', checkJWT , manager,  getUser)
 
 /**
  * @swagger
@@ -100,8 +97,7 @@ router.get('/user', checkJWT, manager, getUser);
  *                   description: Success message.
  *                   example: "User deleted successfully"
  */
-
-router.delete('/user', checkJWT, manager, deleteUser);
+router.delete('/user', checkJWT , manager,  deleteUser);
 
 /**
  * @swagger
@@ -141,303 +137,51 @@ router.delete('/user', checkJWT, manager, deleteUser);
  *                   description: The user's status.
  *                   example: "user"
  */
+router.patch('/userWithoutPassword', checkJWT , manager, MPV.updateWithoutPassword, updateUser);
 
-router.patch('/userWithoutPassword', checkJWT, manager, MPV.updateWithoutPassword, updateUser);
 
-/**
- * @swagger
- * /manager/user:
- *   patch:
- *     summary: Update a user
- *     description: Update a user's information.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *                 description: The user's email.
- *                 example: "alice.smith@gmail.com"
- *               password:
- *                 type: string
- *                 description: The user's password.
- *                 example: "newpassword123"
- *     responses:
- *       200:
- *         description: User updated successfully.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: integer
- *                   description: The user ID.
- *                   example: 1
- *                 email:
- *                   type: string
- *                   description: The user's email.
- *                   example: "alice.smith@gmail.com"
- *                 status:
- *                   type: string
- *                   description: The user's status.
- *                   example: "user"
- */
-
-router.patch('/user', MPV.user, checkJWT, manager, updateUser);
-
-/**
- * @swagger
- * /manager/user:
- *   post:
- *     summary: Create a new user
- *     description: Create a new user in the database.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *                 description: The user's email.
- *                 example: "alice.smith@gmail.com"
- *               password:
- *                 type: string
- *                 description: The user's password.
- *                 example: "password123"
- *     responses:
- *       201:
- *         description: User created successfully.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: integer
- *                   description: The user ID.
- *                   example: 1
- *                 email:
- *                   type: string
- *                   description: The user's email.
- *                   example: "alice.smith@gmail.com"
- *                 status:
- *                   type: string
- *                   description: The user's status.
- *                   example: "user"
- */
-
-router.post('/user', MPV.user, checkJWT, manager, createUser);
-
-/**
- * @swagger
- * /manager/userslist:
- *   get:
- *     summary: Retrieve a list of user names
- *     description: Retrieve a list of user names from the database.
- *     responses:
- *       200:
- *         description: A list of user names.
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                     description: The user ID.
- *                     example: 1
- *                   name:
- *                     type: string
- *                     description: The user's name.
- *                     example: "Alice Smith"
- */
-
-router.get('/userslist', checkJWT, manager, getUsersName);
-
+router.patch('/user', MPV.user,  checkJWT , manager, updateUser);
+router.post('/user', MPV.user,  checkJWT , manager, createUser);
+router.get('/userslist',checkJWT , manager, getUsersName);
 // Routes pour les vouchers
-
-/**
- * @swagger
- * /manager/vouchers:
- *   get:
- *     summary: Retrieve a list of vouchers
- *     description: Retrieve a list of vouchers from the database.
- *     responses:
- *       200:
- *         description: A list of vouchers.
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                     description: The voucher ID.
- *                     example: 1
- *                   name:
- *                     type: string
- *                     description: The name of the voucher.
- *                     example: "Discount Voucher"
- *                   discount:
- *                     type: number
- *                     description: The discount percentage.
- *                     example: 10
- */
-
-router.get('/vouchers', checkJWT, admin, getVouchers);
-
-/**
- * @swagger
- * /manager/voucher:
- *   get:
- *     summary: Retrieve a specific voucher
- *     description: Retrieve a specific voucher from the database.
- *     responses:
- *       200:
- *         description: A voucher object.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: integer
- *                   description: The voucher ID.
- *                   example: 1
- *                 name:
- *                   type: string
- *                   description: The name of the voucher.
- *                   example: "Discount Voucher"
- *                 discount:
- *                   type: number
- *                   description: The discount percentage.
- *                   example: 10
- */
-
-router.get('/voucher', checkJWT, admin, getVoucher);
-
-/**
- * @swagger
- * /manager/voucher:
- *   post:
- *     summary: Create a new voucher
- *     description: Create a new voucher in the database.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *                 description: The name of the voucher.
- *                 example: "Discount Voucher"
- *               discount:
- *                 type: number
- *                 description: The discount percentage.
- *                 example: 10
- *     responses:
- *       201:
- *         description: Voucher created successfully.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: integer
- *                   description: The voucher ID.
- *                   example: 1
- *                 name:
- *                   type: string
- *                   description: The name of the voucher.
- *                   example: "Discount Voucher"
- *                 discount:
- *                   type: number
- *                   description: The discount percentage.
- *                   example: 10
- */
-
-router.post('/voucher', checkJWT, admin, voucherUpload.single('picture'), createVoucher);
-
-/**
- * @swagger
- * /manager/voucher:
- *   delete:
- *     summary: Delete a voucher
- *     description: Delete a voucher from the database.
- *     responses:
- *       200:
- *         description: Voucher deleted successfully.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   description: Success message.
- *                   example: "Voucher deleted successfully"
- */
-
-router.delete('/voucher', checkJWT, admin, deleteVoucher);
-
-/**
- * @swagger
- * /manager/voucher:
- *   patch:
- *     summary: Update a voucher
- *     description: Update a voucher's information.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *                 description: The name of the voucher.
- *                 example: "Discount Voucher"
- *               discount:
- *                 type: number
- *                 description: The discount percentage.
- *                 example: 10
- *     responses:
- *       200:
- *         description: Voucher updated successfully.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: integer
- *                   description: The voucher ID.
- *                   example: 1
- *                 name:
- *                   type: string
- *                   description: The name of the voucher.
- *                   example: "Discount Voucher"
- *                 discount:
- *                   type: number
- *                   description: The discount percentage.
- *                   example: 10
- */
-
-router.patch('/voucher', checkJWT, admin, voucherUpload.single('picture'), updateVoucher);
+router.get('/vouchers',checkJWT , admin, getVouchers);
+router.get('/voucher',checkJWT , admin, getVoucher);
+//router.post('/voucher', voucherUpload.single('picture'), MPV.voucher, createVoucher);
+router.post('/voucher',checkJWT , admin, voucherUpload.single('picture'), createVoucher);
+router.delete('/voucher',checkJWT , admin, deleteVoucher);
+router.patch('/voucher',checkJWT , admin, voucherUpload.single('picture'), updateVoucher);
+//router.patch('/voucher', voucherUpload.single('picture'), MPV.voucher, updateVoucher);
 
 // Routes pour les vouchers réclamés
+router.get('/user-vouchers', checkJWT , admin, getUserVouchers);
+router.get('/user-voucher', checkJWT , admin, getUserVoucher);
+router.post('/user-voucher', checkJWT , admin, MPV.userVoucher, createUserVoucher);
+router.delete('/user-voucher', checkJWT , admin, deleteUserVoucher);
+router.patch('/user-voucher', checkJWT , admin, MPV.userVoucher, updateUserVoucher);
+
+router.get('/employees', checkJWT , manager, getEmployees);
+router.get('/employee', checkJWT , manager,  getEmployee);
+router.delete('/employee', checkJWT , manager, deleteEmployee);
+router.patch('/employee', checkJWT , manager,  MPV.employee, updateEmployee);
+router.get('/employeeslist', checkJWT , manager, getEmployeesName);
+
+router.get('/all-reports',checkJWT , manager, getAllReports);
+router.delete('/all-reports',checkJWT , manager, deleteReport);
+router.get('/all-report',checkJWT , manager, getAllReport);
+//router.patch('/all-report',reportUpload.single('picture'), MPV.report, updateReport);
+router.patch('/all-report',checkJWT , manager,reportUpload.single('picture'), updateReport);
+//router.post('/all-report',reportUpload.single('picture'), MPV.report, createReport);
+router.post('/all-report',checkJWT , manager,reportUpload.single('picture'), createReport);
+
+router.get('/jobs',checkJWT , employee, getJobs);
+router.get('/job',checkJWT , employee, getJob);
+router.patch('/job',checkJWT , employee, reportUpload.single('picture'), updateJob);
+
+router.get('/reporttype',checkJWT , employee, getAllReportType);
+
+router.get('/all-report-types', checkJWT , admin, getReportTypes);
+router.patch('/all-report-type', checkJWT , admin, MPV.reportType,updateReportType);
+router.post('/all-report-type', checkJWT , admin,MPV.reportType, createReportType);
+router.delete('/all-report-type',checkJWT , admin, deleteReportType);
 
 export default router;
